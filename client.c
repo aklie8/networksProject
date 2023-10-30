60502a562ea0200261c21572a96ae37f9174bdbc
aklie8 <potc55bhu@gmail.com>
2023-10-29 18:25:15 -0700

    starting part 1

---
+++ client.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char * argv[]){
+  if(argc != 2){
+    printf("Expected 1 arg (the JSON config file)\n");
+    return 1;
+  }
+
+  char * file_path = argv[1];
+  printf("%s\n", file_path);
+
+  FILE *config_file = fopen(file_path, "r");
+
+  if(config_file == NULL){
+    printf("The file can not be opened\n");
+    return 1;
+  }
+
+  fseek(config_file, 0L, SEEK_END);
+  int file_size = ftell(config_file);
+  rewind(config_file);
+
+  char * data = (char *) calloc(1, file_size + 1);
+ 
+  if(fread(data, file_size, 1, config_file) != 1){
+    printf("failed to read file\n");
+    return 1;
+  }
+
+  fclose(config_file);
+
+  printf("%s\n", data);
+  
+  free(data);
+}
+
--- server.c
+++ server.c
@@ -1,4 +1,17 @@
-#include"stdio.h"
-int main(){
-printf("Hello world /n");
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char * argv[]){
+  if(argc != 2){
+    printf("Expected 1 arg (the TCP Port)\n");
+    return 1;
+  }
+
+  int TCP_port = atoi(argv[1]);
+ // printf("%d\n", TCP_port);
+
+  if(TCP_port > 65535 || TCP_port <= 0){
+    printf("Invalid Port\n");
+  }
+
 }
