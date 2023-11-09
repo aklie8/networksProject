f103e08131725766050976b859d813a280de6e44
aklie8 <potc55bhu@gmail.com>
2023-11-09 10:25:49 -0800

    working on part 1 of part 1

--- common.c
+++ common.c
@@ -204,16 +204,11 @@ int createClientTCPSocket(char * port, char * ip_addr)
 	int rv;
 	char s[INET6_ADDRSTRLEN];
 
-	if (argc != 2) {
-	    fprintf(stderr,"usage: client hostname\n");
-	    exit(1);
-	}
-
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(ip_addr, port, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
